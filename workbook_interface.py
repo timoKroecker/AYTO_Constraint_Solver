801c57c17535febf0c94f26630b4c8a876a8a096
timoKroecker <t.kroecker@web.de>
2024-09-15 09:34:59 +0200

    coloring the matching nights

--- workbook_interface.py
+++ workbook_interface.py
@@ -4,8 +4,13 @@ import colorsys
 NUM_GIRLS = 11
 NUM_BOYS = 10
 
+# [first row/col, last row/col + 1, step_size]
+WB_NIGHTS = [19, 29, 1]
+WB_COUPLES = [98, 117, 2]
+
 RED = "FF0000"
 GREEN = "00A933"
+YELLOW = "FFEE78"
 WHITE = "000000"
 
 def load_worksheet(path, sheet_name):
@@ -53,13 +58,13 @@ def load_input(path):
     girls, boys = get_name_dictionaries(sheet)
     matching_nights = []
     lights = []
-    for night in range(19, 29, 1):
+    for night in range(WB_NIGHTS[0], WB_NIGHTS[1], WB_NIGHTS[2]):
         light = sheet["v" + str(night)].value
         if(light == None):
             break
         lights.append(int(light))
         row = []
-        for i in range(98, 117, 2):
+        for i in range(WB_COUPLES[0], WB_COUPLES[1], WB_COUPLES[2]):
             girl_name = sheet[chr(i + 1) + str(night)].value.replace(" ", "")
             boy_name = sheet[chr(i) + str(night)].value.replace(" ", "")
             row.append([girls[girl_name], boys[boy_name]])
@@ -67,11 +72,6 @@ def load_input(path):
     
 
     return input_matrix, matching_nights, lights
-
-def hex_to_hsv(hex_color):
-    rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    hsv = colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2])
-    return hsv
 
 def hex_to_rgb(hex_color):
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
@@ -89,9 +89,10 @@ def diverging_map(value, domain, range_start, range_mid, range_end):
 def get_hex_color(probability):
     color = [0, 0, 0]
     red = hex_to_rgb(RED)
+    yellow = hex_to_rgb(YELLOW)
     green = hex_to_rgb(GREEN)
     for i in range(len(color)):
-        color[i] = int(diverging_map(probability, [0, 1], red[i], 255, green[i]))
+        color[i] = int(diverging_map(probability, [0, 1], red[i], yellow[i], green[i]))
     return '%02x%02x%02x' % tuple(color)
 
 def write_probabilities(path, probabilities):
@@ -106,4 +107,23 @@ def write_probabilities(path, probabilities):
             fill_color = openpyxl.styles.colors.Color(rgb='00' + hex_color)
             fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=fill_color)
             sheet[cell_index].fill = fill
+
+    girls, boys = get_name_dictionaries(sheet)
+    for night in range(WB_NIGHTS[0], WB_NIGHTS[1], WB_NIGHTS[2]):
+        for i in range(WB_COUPLES[0], WB_COUPLES[1], WB_COUPLES[2]):
+            girl_index = chr(i + 1) + str(night)
+            boy_index = chr(i) + str(night)
+            girl_name = sheet[girl_index].value
+            boy_name = sheet[boy_index].value
+            if(girl_name == None or boy_name == None):
+                continue
+            girl_name = girl_name.replace(" ", "")
+            boy_name = boy_name.replace(" ", "")
+            cell_index = get_cell_index(girls[girl_name] - 1, boys[boy_name] - 1)
+            hex_color = sheet[cell_index].fill.fgColor.index[2:]
+            fill_color = openpyxl.styles.colors.Color(rgb='00' + hex_color)
+            fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=fill_color)
+            sheet[girl_index].fill = fill
+            sheet[boy_index].fill = fill
+    
     book.save(path)
